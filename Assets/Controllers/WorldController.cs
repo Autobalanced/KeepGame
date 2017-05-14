75754579d1cd277fd4a3eeecaa7de1ed2ebbdb42
Autobalanced <scwables@gmail.com>
2017-05-14 17:07:11 +1000

    TODO: Finish WorldController

--- Assets/Controllers/WorldController.cs
+++ Assets/Controllers/WorldController.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class WorldController : MonoBehaviour {
 
+    public static WorldController Instance { get; protected set; }
+
+    // Set the Sprites for Tiles
+    public Sprite floorSprite;
+    public Sprite emptySprite;
+
+    // Create a collection of all Tiles and Buildings and their related GameObjects
+    Dictionary<Tile, GameObject> tileGameObjectMap;
+    Dictionary<Building, GameObject> buildingGameObjectMap;
+
+    // Dictionary of all Buildings and their associated Sprites
+    Dictionary<string, Sprite> buildingSprites;
+
+    // The full world generated
+    public World world { get; protected set; }
+
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
